b973ee52c69327ab254b9e238f3c8e383df5ff7d
Omar Furrer <omar.furrer@gmail.com>
2018-04-08 17:22:58 +0200

    Add custom css file

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,6 +19,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <link rel="stylesheet" href="{{ asset('adminlte/css/all.css') }}">
         <link rel="stylesheet" href="{{ asset('adminlte/dist/css/AdminLTE.min.css') }}">
         <link rel="stylesheet" href="{{ asset('adminlte/dist/css/skins/skin-blue.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
