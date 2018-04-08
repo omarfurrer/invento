fedc5876afaedcc3fc586345e3f46da9795c64d0
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-04-08 15:35:11 +0200

    Added table view for users and action icons

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,8 +7,8 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
     <h1>
-        Page Header
-        <small>Optional description</small>
+        Add New user
+        <a href="#"><i class="fa fa-plus" aria-hidden="true">  </i></a>
     </h1>
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -17,13 +17,46 @@
 </section>
 
 <!-- Main content -->
-<section class="content container-fluid">
 
-    <!--------------------------
-    | Your Page Content Here |
-    -------------------------->
-    
-  
+
+<section class="content container-fluid">
+    <div class="row">
+
+
+        <table class="table">
+
+
+            <thead class="thead-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Role</th>
+              </tr>
+          </thead>
+          <tbody>
+
+            @for ($i = 0; $i < count($users); $i++)
+
+            <tr>
+              <th scope="row">{{$users[$i]->id}}</th>
+              <td>{{ $users[$i]->name }}</td>
+              <td>{{ $users[$i]->email }}</td>
+              <td>{{ $users[$i]->roles()->first()->name }}</td>
+
+              <td><ol class="list-inline">
+                <li><a href="#"><i class="fa fa-trash" aria-hidden="true"></i> Delete</a></li>
+                <li><a href="#"><i class="fa fa-pencil" aria-hidden="true"> </i> Edit</a></li>    
+            </ol></td>
+
+        </tr>
+    </tbody>
+    @endfor
+</table>
+
+
+</div>
+
 
 
 
