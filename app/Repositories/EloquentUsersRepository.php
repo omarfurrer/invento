15273783828eb8e79e8da64243acd1ea0f9a34fd
Omar Furrer <omar.furrer@gmail.com>
2018-04-09 17:57:52 +0200

    Updated users repository

--- app/Repositories/EloquentUsersRepository.php
+++ app/Repositories/EloquentUsersRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Repositories\Interfaces\UsersRepositoryInterface;
 use App\User;
+use Spatie\Permission\Models\Role;
 
 class EloquentUsersRepository extends EloquentAbstractRepository implements UsersRepositoryInterface {
 
@@ -19,15 +20,45 @@ class EloquentUsersRepository extends EloquentAbstractRepository implements User
      * Create a new user.
      *
      * @param array $fields
+     * @param string|integer $role
      * @return mixed
      */
     public function create(array $fields = null, $role = 'employee')
     {
         if (!empty($fields['password'])) {
             $fields['password'] = bcrypt($fields['password']);
+        } else {
+            $fields['password'] = bcrypt('12345678');
         }
 
         $user = parent::create($fields);
+
+        if (is_numeric($role)) {
+            $role = Role::findById($role)->name;
+        }
+
+        $user->assignRole($role);
+
+        return $user;
+    }
+
+    /**
+     * Update a user.
+     *
+     * @param Integer $id
+     * @param array $fields
+     * @param string|integer $role
+     * @return mixed
+     */
+    public function update($id, array $fields = array(), $role = 'employee')
+    {
+        $user = parent::update($id, $fields);
+
+        $user->roles()->detach();
+
+        if (is_numeric($role)) {
+            $role = Role::findById($role)->name;
+        }
 
         $user->assignRole($role);
 
