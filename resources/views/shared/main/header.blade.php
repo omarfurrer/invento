cf9a12e0cc41aaad067a715cd74f41de4d7c08f7
Omar Furrer <omar.furrer@gmail.com>
2018-04-17 18:24:19 +0200

    Add user name & image using gravatar in header and aside

--- resources/views/shared/main/header.blade.php
+++ resources/views/shared/main/header.blade.php
@@ -120,9 +120,9 @@
                     <!-- Menu Toggle Button -->
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <!-- The user image in the navbar-->
-                        <img src="{{ asset("adminlte/dist/img/user2-160x160.jpg") }}" class="user-image" alt="User Image">
+                        <img src="{{ Auth::user()->gravatar }}" class="user-image" alt="User Image">
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
