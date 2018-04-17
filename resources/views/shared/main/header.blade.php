908ffc11cf34f69336d180eba3f5ba956f99f830
Omar Furrer <omar.furrer@gmail.com>
2018-04-17 18:32:29 +0200

    UI cleaning and adjustments

--- resources/views/shared/main/header.blade.php
+++ resources/views/shared/main/header.blade.php
@@ -5,7 +5,7 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini">{{ config('app.name') }}</span>
+        <span class="logo-mini">{{ config('app.name')[0] }}</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg">{{ config('app.name') }}</span>
     </a>
@@ -20,8 +20,8 @@
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
                 <!-- Messages: style can be found in dropdown.less-->
-                <li class="dropdown messages-menu">
-                    <!-- Menu toggle button -->
+<!--                <li class="dropdown messages-menu">
+                     Menu toggle button 
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-envelope-o"></i>
                         <span class="label label-success">4</span>
@@ -29,35 +29,35 @@
                     <ul class="dropdown-menu">
                         <li class="header">You have 4 messages</li>
                         <li>
-                            <!-- inner menu: contains the messages -->
+                             inner menu: contains the messages 
                             <ul class="menu">
-                                <li><!-- start message -->
+                                <li> start message 
                                     <a href="#">
                                         <div class="pull-left">
-                                            <!-- User Image -->
+                                             User Image 
                                             <img src="{{ asset("adminlte/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
                                         </div>
-                                        <!-- Message title and timestamp -->
+                                         Message title and timestamp 
                                         <h4>
                                             Support Team
                                             <small><i class="fa fa-clock-o"></i> 5 mins</small>
                                         </h4>
-                                        <!-- The message -->
+                                         The message 
                                         <p>Why not buy a new awesome theme?</p>
                                     </a>
                                 </li>
-                                <!-- end message -->
+                                 end message 
                             </ul>
-                            <!-- /.menu -->
+                             /.menu 
                         </li>
                         <li class="footer"><a href="#">See All Messages</a></li>
                     </ul>
                 </li>
-                <!-- /.messages-menu -->
+                 /.messages-menu 
 
-                <!-- Notifications Menu -->
+                 Notifications Menu 
                 <li class="dropdown notifications-menu">
-                    <!-- Menu toggle button -->
+                     Menu toggle button 
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-bell-o"></i>
                         <span class="label label-warning">10</span>
@@ -65,22 +65,22 @@
                     <ul class="dropdown-menu">
                         <li class="header">You have 10 notifications</li>
                         <li>
-                            <!-- Inner Menu: contains the notifications -->
+                             Inner Menu: contains the notifications 
                             <ul class="menu">
-                                <li><!-- start notification -->
+                                <li> start notification 
                                     <a href="#">
                                         <i class="fa fa-users text-aqua"></i> 5 new members joined today
                                     </a>
                                 </li>
-                                <!-- end notification -->
+                                 end notification 
                             </ul>
                         </li>
                         <li class="footer"><a href="#">View all</a></li>
                     </ul>
                 </li>
-                <!-- Tasks Menu -->
+                 Tasks Menu 
                 <li class="dropdown tasks-menu">
-                    <!-- Menu Toggle Button -->
+                     Menu Toggle Button 
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-flag-o"></i>
                         <span class="label label-danger">9</span>
@@ -88,18 +88,18 @@
                     <ul class="dropdown-menu">
                         <li class="header">You have 9 tasks</li>
                         <li>
-                            <!-- Inner menu: contains the tasks -->
+                             Inner menu: contains the tasks 
                             <ul class="menu">
-                                <li><!-- Task item -->
+                                <li> Task item 
                                     <a href="#">
-                                        <!-- Task title and progress text -->
+                                         Task title and progress text 
                                         <h3>
                                             Design some buttons
                                             <small class="pull-right">20%</small>
                                         </h3>
-                                        <!-- The progress bar -->
+                                         The progress bar 
                                         <div class="progress xs">
-                                            <!-- Change the css width attribute to simulate progress -->
+                                             Change the css width attribute to simulate progress 
                                             <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
                                                  aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
                                                 <span class="sr-only">20% Complete</span>
@@ -107,14 +107,14 @@
                                         </div>
                                     </a>
                                 </li>
-                                <!-- end task item -->
+                                 end task item 
                             </ul>
                         </li>
                         <li class="footer">
                             <a href="#">View all tasks</a>
                         </li>
                     </ul>
-                </li>
+                </li>-->
                 <!-- User Account Menu -->
                 <li class="dropdown user user-menu">
                     <!-- Menu Toggle Button -->
@@ -127,15 +127,15 @@
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
                         <li class="user-header">
-                            <img src="{{ asset("adminlte/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
+                            <img src="{{ Auth::user()->gravatar }}" class="img-circle" alt="User Image">
 
                             <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2012</small>
+                                {{ Auth::user()->name }} - {{ Auth::user()->roles[0]->name }}
+                                <small>Member since {{ Auth::user()->created_at->diffForHumans() }}</small>
                             </p>
                         </li>
                         <!-- Menu Body -->
-                        <li class="user-body">
+<!--                        <li class="user-body">
                             <div class="row">
                                 <div class="col-xs-4 text-center">
                                     <a href="#">Followers</a>
@@ -147,12 +147,12 @@
                                     <a href="#">Friends</a>
                                 </div>
                             </div>
-                            <!-- /.row -->
-                        </li>
+                             /.row 
+                        </li>-->
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                <a href="#" class="btn btn-default btn-flat disabled">Profile</a>
                             </div>
                             <div class="pull-right">
                                 <a href="#" class="btn btn-default btn-flat"
@@ -167,9 +167,9 @@
                     </ul>
                 </li>
                 <!-- Control Sidebar Toggle Button -->
-                <li>
+<!--                <li>
                     <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                </li>
+                </li>-->
             </ul>
         </div>
     </nav>
