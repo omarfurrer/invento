d85a7a2116720f4eae0032ae88568c8ebc2a1873
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-05-15 16:21:36 +0200

    Fix Dashboard minor fixes

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,78 +6,87 @@
 @section('content')
 <!-- Content Header (Page header) -->
 <section class="content-header">
-    <h1>
-        Dashboard
-    </h1>
+  <h1>
+
+    <b>Important Alerts</b>
+     <i class="fa fa-info-circle"></i>
+  </h1>
 
 </section>
 
 <!-- Main content -->
 <section class="content container-fluid">
-    <div class="row">
-        
-        @if(!$lowQuantityItems->isEmpty())
-            <h3 class="dashboardHeaders"><b>Low Quantity Items</b></h3>
-        @foreach($lowQuantityItems as $item)
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            
-          <div class="info-box">
-            <span class="info-box-icon bg-blue"><i class="fa fa-exclamation"></i></span>
+<div class="row">
+  <div class="col-sm-12">
 
-            <div class="info-box-content">
-              <span class="info-box-text">{{ $item->description }}</span>
-              <span class="info-box-number">Remaining: {{ $item->current_quantity }} {{ $item->measurementUnit->short_name }}</span>
-          </div>
-          <!-- /.info-box-content -->
+    @if(!$lowQuantityItems->isEmpty())
+       <div class="row">
+      <h3 class="dashboardHeaders">Low Quantity Items</h3>
+      @foreach($lowQuantityItems as $item)
+      <div class="col-md-4 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-blue"><i class="fa fa-exclamation"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">{{ $item->description }}</span>
+          <span class="info-box-number">Remaining: {{ $item->current_quantity }} {{ $item->measurementUnit->short_name }}</span>
+        </div>
+        <!-- /.info-box-content -->
       </div>
       <!-- /.info-box -->
       
+    </div>
+    @endforeach
+
+
+    <!-- /.table-responsive -->
+  </div>
+
+  <!-- /.box-body -->
+
+  @endif
+
+    @if(!$expiringSoonItems->isEmpty())
+    <div class="row">
+    <h3 class="dashboardHeaders">Expiring Soon Items</h3>
+    @foreach($expiringSoonItems as $item)
+     <div class="col-md-4 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-orange"><i class="fa fa-exclamation"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">{{ $item->description }}</span>
+          <span class="info-box-number">Expires on: {{ $item->expiry_date }}</span>
+          <span class="info-box-number smallFontSpan">Remaining: {{ $item->current_quantity }} {{ $item->short_name }}</span>
+        </div>
+      </div> 
+    </div>
+    @endforeach
+
+  </div>
+   @endif
+
+
+   @if(!$expiredItems->isEmpty())
+   <div class="row">
+  <h3 class="dashboardHeaders">Expired Items</h3>
+   @foreach($expiredItems as $item)
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="info-box">
+      <span class="info-box-icon bg-red"><i class="fa fa-exclamation"></i></span>
+      <div class="info-box-content">
+        <span class="info-box-text">{{ $item->description }}</span>
+        <span class="info-box-number">Expired on: {{ $item->expiry_date }}</span>
+        <span class="info-box-number smallFontSpan">Remaining: {{ $item->current_quantity }} {{ $item->short_name }}</span>
+      </div>
+    </div> 
   </div>
   @endforeach
   
-  
-<!-- /.table-responsive -->
 </div>
-
-<!-- /.box-body -->
-
 @endif
 
-@if(!$expiringSoonItems->isEmpty())
-       <div class="row">
-     <h3 class="dashboardHeaders"><b>Expiring Soon Items</b></h3>
-@foreach($expiringSoonItems as $item)
-          <div class="col-md-4 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-orange"><i class="fa fa-exclamation"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">{{ $item->description }}</span>
-              <span class="info-box-number">Expires on: {{ $item->expiry_date }}</span>
-              <span class="info-box-number smallFontSpan">Remaining: {{ $item->current_quantity }} {{ $item->short_name }}</span>
-          </div>
-      </div> 
-  </div>
-@endforeach
-@endif
+
+
 </div>
-
-
-@if(!$expiredItems->isEmpty())
-       <div class="row">
-     <h3 class="dashboardHeaders"><b>Expired Items</b></h3>
-@foreach($expiredItems as $item)
-          <div class="col-md-4 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-exclamation"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">{{ $item->description }}</span>
-              <span class="info-box-number">Expired on: {{ $item->expiry_date }}</span>
-              <span class="info-box-number smallFontSpan">Remaining: {{ $item->current_quantity }} {{ $item->short_name }}</span>
-          </div>
-      </div> 
-  </div>
-@endforeach
-@endif
 </div>
 </section>
 @endsection
