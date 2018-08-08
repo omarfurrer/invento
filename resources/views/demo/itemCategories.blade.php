4f927699122a43a7e8eae6005f618e278e2a3b91
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-08-08 18:41:20 +0200

    Added basic view for categories

--- resources/views/demo/itemCategories.blade.php
+++ resources/views/demo/itemCategories.blade.php
@@ -4,8 +4,194 @@
 
 @section('content')
 
-<div>Content Goes here</div>
+<section class="content container-fluid">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="box">
+
+				<!-- BOX HEADER -->
+
+				<div class="box-header">
+					<h5 class="box-title">Showing: <b>5 Items</b></h5>
+
+					<div class="box-tools">
+						<div class="input-group input-group-sm searchInput">
+							<input type="text" name="table_search" class="form-control pull-right" placeholder="Search" id="search" onkeyup="search()">
+
+							<div class="input-group-btn">
+								<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- End of BOX HEADER -->
+
+				<!--  BOX BODY -->
+
+				<div class="box-body table-responsive no-padding">
+					<table class="table table-hover" id="itemsTbl">
+
+						<tbody>
+
+							<tr id="tHeaderRow">
+								<th>Image</th>
+								<th>Item</th>
+								<th>Quantity</th>
+								<th>Remainig Uses</th>
+								<th>Withdraw <i class="fa fa-info-circle" aria-hidden="true" data-toggle="tooltip" title="You can choose to withdraw by a single use or by setting a custom amount of uses you want to withdraw"></i></th>
+
+							</tr>
+
+							<tr class="categoryRow">
+								<td id="categoryTd" colspan="5">
+									<button class="btn" data-toggle="collapse" data-target=".multi-collapse" >ORTHO
+										<a><i class="fa fa-chevron-down pull-right"></i></a>
+									</button>
+									
+								</td>
+							</tr>
+
+							<tr class="collapse multi-collapse">
+								<td><img src="https://placeholdit.co//i/50x50?"></td>
+								<td>Temporary Cement</td>
+								<td>3</td>
+								<td>20</td>
+								<td>
+									<ul class="list-inline">
+
+										<li class="list-inline-item"><button class="btn btn-default btn-sm minusOneBtn"><i class="fa fa-chevron-down fa-sm"></i> -1</button></li>
+
+										<li class="list-inline-item">
+											<button class="btn btn-sm btn-default" data-toggle="collapse" data-target="#customInput1"><i class="fa fa-pencil"></i></button>
+										</li>
+										<li class="list-inline-item"><input class="collapse" type="number" id="customInput1"></li>
+									</ul>
+								</td>
+							</tr>
+
+							<tr class="collapse multi-collapse">
+								<td><img src="https://placeholdit.co//i/50x50?"></td>
+								<td>Suction</td>
+								<td>5</td>
+								<td value="10" class="remaining">10</td>
+								<td>
+
+									<ul class="list-inline">
+										<li class="list-inline-item"><button class="btn btn-default btn-sm minusOneBtn"><i class="fa fa-chevron-down fa-sm"></i> -1</button></li>
+
+										<li class="list-inline-item">
+											<button class="btn btn-sm btn-default" data-toggle="collapse" data-target="#customInput2"><i class="fa fa-pencil"></i></button>
+										</li>
+										<li class="list-inline-item"><input class="collapse" type="number" id="customInput2"></li>
+									</ul>
+
+								</td>
+							</tr>
+
+							<tr class="collapse multi-collapse">
+								<td><img src="https://placeholdit.co//i/50x50?"></td>
+								<td>Matrix Band Small</td>
+								<td>4</td>
+								<td>9</td>
+								<td>
+
+									<ul class="list-inline">
+										<li class="list-inline-item"><button class="btn btn-default btn-sm minusOneBtn"><i class="fa fa-chevron-down fa-sm"></i> -1</button></li>
+
+										<li class="list-inline-item">
+											<button class="btn btn-sm btn-default" data-toggle="collapse" data-target="#customInput3"><i class="fa fa-pencil"></i></button>
+										</li>
+										<li class="list-inline-item"><input class="collapse" type="number" id="customInput3"></li>
+									</ul>
+									
+								</td>
+							</tr>
+
+							<tr class="collapse multi-collapse">
+								<td><img src="https://placeholdit.co//i/50x50?"></td>
+								<td>Hexitol</td>
+								<td>6</td>
+								<td>13</td>
+								<td>
+
+									<ul class="list-inline">
+										<li class="list-inline-item"><button class="btn btn-default btn-sm minusOneBtn"><i class="fa fa-chevron-down fa-sm"></i> -1</button></li>
+
+										<li class="list-inline-item">
+											<button class="btn btn-sm btn-default" data-toggle="collapse" data-target="#customInput4"><i class="fa fa-pencil"></i></button>
+										</li>
+										<li class="list-inline-item"><input class="collapse" type="number" id="customInput4"></li>
+									</ul>
+									
+								</td>
+							</tr>
+
+						</tbody>
+
+					</table>
 
 
+				</div>
+				<!-- /.box-body -->
+
+
+			</div>
+		</div>
+	</div> 
+
+</section>
 
 @endsection
+
+@push('scripts')
+
+<script>
+
+	function search() {
+
+		var input, filter, table, tr, td, i;
+		input = document.getElementById("search");
+		filter = input.value.toUpperCase();
+		table = document.getElementById("itemsTbl");
+		tr = table.getElementsByTagName("tr");
+
+		for (i = 0; i < tr.length; i++) {
+			td = tr[i].getElementsByTagName("td")[1];
+			if (td) {
+				if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+					tr[i].style.display = "";
+				} else {
+					tr[i].style.display = "none";
+				}
+			}
+		}
+	}
+
+
+
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+
+		var totalUses = $(".remaining").html();
+
+		
+		
+
+
+	$(".minusOneBtn").click(function() {
+		
+		var subtractedUses = totalUses - 1;
+		$(this).next(".remaining").replaceWith("subtractedUses");
+		
+	   });
+
+	});
+
+     
+
+
+</script>
+
+@endpush
